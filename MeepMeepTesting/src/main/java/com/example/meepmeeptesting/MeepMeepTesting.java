d39b444f695935907074502e4ecd36f89ca3d317
AmalMadapaty <amalmadapaty@gmail.com>
2023-11-15 16:00:31 -0600

    MeepMeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -37,11 +37,30 @@ public class MeepMeepTesting {
                                 .build()
                         */
 
+                        /*
                         //RedPark2
                         drive.trajectorySequenceBuilder(new Pose2d(12, -60.5, Math.toRadians(90)))
                                 .strafeRight(48)
                                 .build()
+                        */
 
+                        //BlueBackboard1
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 60.5, Math.toRadians(-90)))
+                                .forward(49)
+                                .strafeLeft(50)
+                                .splineToLinearHeading(new Pose2d(40, 35, Math.toRadians(180)), Math.toRadians(0))
+                                .back(9)
+                                .build()
+
+                        /*
+                        //RedBackboard1
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, -60.5, Math.toRadians(90)))
+                                .forward(49)
+                                .strafeRight(50)
+                                .splineToLinearHeading(new Pose2d(40, -35, Math.toRadians(180)), Math.toRadians(0))
+                                .back(9)
+                                .build()
+                         */
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
