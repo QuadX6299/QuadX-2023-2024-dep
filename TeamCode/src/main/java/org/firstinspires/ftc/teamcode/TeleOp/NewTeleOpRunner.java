d5eb13416bc6f1b0e9e519b1786a6791c21530dd
AmalMadapaty <amalmadapaty@gmail.com>
2023-12-04 20:06:43 -0600

    Pushed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NewTeleOpRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NewTeleOpRunner.java
@@ -4,19 +4,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name="NewTeleOpRunner", group="Iterative OpMode")
 
+/*
 public class NewTeleOpRunner extends FieldOrientedDriveTeleop {
     public void loop() {
         gyroMovement();
         telemetry.update();
     }
 }
+*/
 
-/*
+
 public class NewTeleOpRunner extends NewTeleOp {
     public void loop() {
         mechMovement();
         telemetry.update();
     }
 }
- */
+
 
