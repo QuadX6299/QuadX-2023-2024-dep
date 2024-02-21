dc9ce33556fc3fda95611328572335ac67fdd127
AmalMadapaty <amalmadapaty@gmail.com>
2024-02-21 15:51:28 -0600

    2 Grid Autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/GridAutos/RedRedGrid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/GridAutos/RedRedGrid.java
@@ -9,6 +9,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Auto.Vision.HuskyVision;
+import org.firstinspires.ftc.teamcode.Auto.Vision.VisionTest;
 import org.firstinspires.ftc.teamcode.Auto.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.Auto.trajectorysequence.TrajectorySequence;
 
@@ -39,73 +42,102 @@ public class RedRedGrid extends LinearOpMode {
     public void runOpMode() throws InterruptedException{
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         init(this);
+        HuskyVision husky = new HuskyVision(this);
         Pose2d startPose = new Pose2d(12, -60, Math.toRadians(90));
         drive.setPoseEstimate(startPose);
         // Right Spike
-        Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineToConstantHeading(new Vector2d(38, -28), Math.toRadians(0))
+        Trajectory rtraj1 = drive.trajectoryBuilder(startPose)
+                .splineToConstantHeading(new Vector2d(38, -36), Math.toRadians(90))
                 .addTemporalMarker(1.5, () -> {
                     intake.setPower(-0.4);
                 })
                 .build();
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end(), false)
+        Trajectory rtraj2 = drive.trajectoryBuilder(rtraj1.end(), false)
                 .lineToConstantHeading(new Vector2d(38, -60))
                 .addDisplacementMarker(() -> {
                     intake.setPower(0);
                 })
                 .build();
-         Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
-                .splineToConstantHeading(new Vector2d(57, -26), Math.toRadians(0))
+        Trajectory rtraj3 = drive.trajectoryBuilder(rtraj2.end())
+                .splineToConstantHeading(new Vector2d(62, -41), Math.toRadians(90))
                 .addDisplacementMarker(() -> {
                     servoOuttake.setPosition(0.4);
                 })
                 .build();
-        TrajectorySequence ts1 = drive.trajectorySequenceBuilder(traj3.end())
-                .turn(Math.toRadians(90))
+        Trajectory rtraj4 = drive.trajectoryBuilder(rtraj3.end())
+                .forward(4)
                 .build();
 
         //Middle Spike
-        /*
-        Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .lineToConstantHeading(new Vector2d(12, -18))
+        Trajectory midtraj1 = drive.trajectoryBuilder(startPose)
+                .lineToConstantHeading(new Vector2d(12, -22))
                 .addTemporalMarker(1.5, () -> {
                     intake.setPower(-0.4);
                 })
                 .build();
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end(), false)
+        Trajectory midtraj2 = drive.trajectoryBuilder(midtraj1.end(), false)
                 .lineToConstantHeading(new Vector2d(12, -60))
                 .addDisplacementMarker(() -> {
                     intake.setPower(0);
                 })
                 .build();
-         Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
-                .splineToConstantHeading(new Vector2d(57, -26), Math.toRadians(0))
+        Trajectory midtraj3 = drive.trajectoryBuilder(midtraj2.end())
+                .splineToConstantHeading(new Vector2d(62, -41), Math.toRadians(90))
                 .addDisplacementMarker(() -> {
                     servoOuttake.setPosition(0.4);
                 })
                 .build();
-        TrajectorySequence ts1 = drive.trajectorySequenceBuilder(traj3.end())
-                .turn(Math.toRadians(90))
+        Trajectory midtraj4 = drive.trajectoryBuilder(midtraj3.end())
+                .forward(4)
                 .build();
 
-         */
-        Trajectory traj4 = drive.trajectoryBuilder(ts1.end())
-                .back(7)
+        Trajectory otraj = drive.trajectoryBuilder(startPose)
+                .splineToConstantHeading(new Vector2d(62, -37), Math.toRadians(90))
                 .addDisplacementMarker(() -> {
-                     et.reset();
-                     while (et.milliseconds() < 500) {
-                         liftLeft.setPower(1);
-                         liftRight.setPower(1);
-                     }
-                 })
+                    servoOuttake.setPosition(0.4);
+                })
+                .build();
+
+        //Left Spike
+        Trajectory ltraj1 = drive.trajectoryBuilder(startPose)
+                .splineToConstantHeading(new Vector2d(38, -36), Math.toRadians(90))
+                .build();
+        TrajectorySequence lts1 = drive.trajectorySequenceBuilder(ltraj1.end())
+                .turn(Math.toRadians(90))
+                .build();
+        Trajectory ltraj2 = drive.trajectoryBuilder(lts1.end())
+                .lineToConstantHeading(new Vector2d(10, -21))
+                .addTemporalMarker(1.5, () -> {
+                    intake.setPower(-0.4);
+                })
+                .build();
+        Trajectory ltraj3 = drive.trajectoryBuilder(ltraj2.end())
+                .splineToConstantHeading(new Vector2d(47, -37), Math.toRadians(180))
+                .addDisplacementMarker(() -> {
+                    intake.setPower(0);
+                    servoOuttake.setPosition(0.4);
+                })
+                .build();
+
+        //omni
+        Pose2d aPose = new Pose2d(57, -32, Math.toRadians(180));
+        Trajectory traj5 = drive.trajectoryBuilder(aPose)
+                .back(7)
+                .addTemporalMarker(2, () -> {
+                    et.reset();
+                    while (et.milliseconds() < 500) {
+                        liftLeft.setPower(1);
+                        liftRight.setPower(1);
+                    }
+                })
                  .build();
-        Trajectory traj5 = drive.trajectoryBuilder(traj4.end())
+        Trajectory traj6 = drive.trajectoryBuilder(traj5.end())
                 .back(2)
                 .addDisplacementMarker(() -> {
                     servoOuttake.setPosition(0.6);
                 })
                 .build();
-        Trajectory traj6 = drive.trajectoryBuilder(traj5.end())
+        Trajectory traj7 = drive.trajectoryBuilder(traj6.end())
                 .forward(1)
                 .addDisplacementMarker(() -> {
                     servoOuttake.setPosition(0.4);
@@ -116,27 +148,46 @@ public class RedRedGrid extends LinearOpMode {
                     }
                 })
                 .build();
-        Trajectory traj7 = drive.trajectoryBuilder(traj6.end())
+        Trajectory traj8 = drive.trajectoryBuilder(traj7.end())
                 .forward(4)
                 .build();
-        Trajectory traj8 = drive.trajectoryBuilder(traj7.end())
-                .strafeLeft(80)
-                .build();
         Trajectory traj9 = drive.trajectoryBuilder(traj8.end())
+                .strafeLeft(50)
+                .build();
+        Trajectory traj10 = drive.trajectoryBuilder(traj9.end())
                 .back(15)
                 .build();
 
+        int num = husky.propPos();
+        telemetry.addData("Pos", num);
+        telemetry.update();
+
         waitForStart();
-        drive.followTrajectory(traj1);
-        sleep(300);
-        drive.followTrajectory(traj2);
-        drive.followTrajectory(traj3);
-        drive.followTrajectorySequence(ts1);
-        drive.followTrajectory(traj4);
+
+        if((num < 0) && (num > -100)) {
+            drive.followTrajectory(midtraj1);
+            sleep(300);
+            drive.followTrajectory(midtraj2);
+            drive.followTrajectory(midtraj3);
+            drive.followTrajectory(midtraj4);
+        } else if (num > 0) {
+            drive.followTrajectory(rtraj1);
+            sleep(300);
+            drive.followTrajectory(rtraj2);
+            drive.followTrajectory(rtraj3);
+            drive.followTrajectory(rtraj4);
+        } else {
+            drive.followTrajectory(ltraj1);
+            drive.followTrajectorySequence(lts1);
+            drive.followTrajectory(ltraj2);
+            drive.followTrajectory(ltraj3);
+        }
+
         drive.followTrajectory(traj5);
         drive.followTrajectory(traj6);
         drive.followTrajectory(traj7);
         drive.followTrajectory(traj8);
         drive.followTrajectory(traj9);
+        drive.followTrajectory(traj10);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Vision/HuskyVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Vision/HuskyVision.java
@@ -6,61 +6,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class HuskyVision extends LinearOpMode {
     HuskyLens huskyLens;
-    private int cy = 0;
+    private int ca = 0;
 
     public HuskyVision(LinearOpMode opMode) throws InterruptedException {
         huskyLens = opMode.hardwareMap.get(HuskyLens.class, "huskyLens");
     }
-    public int bluePropPos(){
+    public int propPos(){
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
-            for (HuskyLens.Block b : huskyLens.blocks()) {
-                if (b.id == 1) {
-                    cy = b.y;
-                }
-                else if (b.id == 2) {
-                    cy = -b.y;
-                } else {
-                    cy = -1;
-                }
+        for (HuskyLens.Block b : huskyLens.blocks()) {
+            if (b.id == 1) {
+                ca = b.y;
+            } else if (b.id == 2) {
+                ca = b.y * -1;
+            } else {
+                ca = -1;
             }
-//        if (cx < 85){
-//            return "LEFT";
-//        }
-//        else if (cx > 85 && cx < 180){
-//            return "MIDDLE";
-//        }
-//        else if (cx > 180){
-//            return "RIGHT";
-//        }
-//        else {
-//            return "null";
-//        }
-        return cy;
+        }
+        return ca;
     }
-    /*
-    public String redPropPos(){
-        for (HuskyLens.Block b : huskyLens.blocks()){
-            if (b.id == 2){
-                cx = b.x;
-            }
-            else{
-                cx = -1;
-            }
-        }
-        if (cx < 85){
-            return "LEFT";
-        }
-        else if (cx > 85 && cx < 180){
-            return "MIDDLE";
-        }
-        else if (cx > 180){
-            return "RIGHT";
-        }
-        else {
-            return "null";
-        }
-    }
-    */
+
     @Override
     public void runOpMode() throws InterruptedException {
     }
